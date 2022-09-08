ece0b7e3b877f6b5a7f3bee074683361bced3eef
Daniel Haiem <dnhaiem@gmail.com>
2022-09-08 20:25:16 +0300

    connected mongodb db

--- server.ts
+++ server.ts
@@ -2,9 +2,10 @@ import express, { Request, Response, Application } from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import connectDB from "./config/db";
 
 dotenv.config();
-//connectDb()
+connectDB();
 const app: Application = express();
 const PORT = process.env.PORT || 5000;
 app.use(cors());
